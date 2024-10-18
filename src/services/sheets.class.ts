0589ab6f2840c234cfc4ad034bcf0b18c1e9bfc9
Diego Reyes <diegorele13@gmail.com>
2024-10-17 19:54:46 -0500

    ad: key

--- src/services/sheets.class.ts
+++ src/services/sheets.class.ts
@@ -1,7 +1,5 @@
-import { HttpStatusCode } from "axios";
 import { JWT } from "google-auth-library";
 import { GoogleSpreadsheet } from "google-spreadsheet";
-import { googleKey } from "../utils/googlePrivateKey";
 
 export default class GoogleSheetService {
   private doc: GoogleSpreadsheet;
@@ -9,7 +7,7 @@ export default class GoogleSheetService {
   constructor() {
     const serviceAccountAuth = new JWT({
       email: process.env.GOOGLE_SERVICE_ACCOUNT_EMAIL,
-      key: googleKey,
+      key: process.env.GOOGLE_PRIVATE_KEY,
       scopes: [
         'https://www.googleapis.com/auth/spreadsheets',
       ]
@@ -22,11 +20,9 @@ export default class GoogleSheetService {
     await this.doc.loadInfo();
   }
 
-  // Buscar la hoja que tenga el nombre del cliente
   async findSheetByClientName(clientName: string): Promise<any> {
     await this.loadDoc();
 
-    // Recorrer todas las hojas y encontrar una que coincida con el nombre del cliente
     const sheet = this.doc.sheetsByIndex.find(sheet => 
       sheet.title.toLowerCase() === clientName.toLowerCase()
     );
@@ -35,14 +31,12 @@ export default class GoogleSheetService {
       throw new Error(`Hoja para el cliente ${clientName} no encontrada`);
     }
 
-    // Obtener las filas de la hoja del cliente encontrada
     const rows = await sheet.getRows();
     
     if (rows.length === 0) {
       throw new Error(`No hay datos en la hoja para el cliente ${clientName}`);
     }
 
-    // Extraemos la información relevante de la hoja
     const clientInfo = rows.map(row => ({
       nombreCliente: row.get('nombre'),
       correoElectronico: row.get('correo electronico'),
