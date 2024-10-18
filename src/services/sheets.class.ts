130a01ec69d94eeaf84e9cfdeba12442be77bcb3
Diego Reyes <diegorele13@gmail.com>
2024-10-17 20:00:28 -0500

    add: replace

--- src/services/sheets.class.ts
+++ src/services/sheets.class.ts
@@ -7,7 +7,7 @@ export default class GoogleSheetService {
   constructor() {
     const serviceAccountAuth = new JWT({
       email: process.env.GOOGLE_SERVICE_ACCOUNT_EMAIL,
-      key: process.env.GOOGLE_PRIVATE_KEY,
+      key: process.env.GOOGLE_PRIVATE_KEY!.replace(/\\n/g, "\n"),
       scopes: [
         'https://www.googleapis.com/auth/spreadsheets',
       ]
